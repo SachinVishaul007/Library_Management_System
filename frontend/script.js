67e23da983e979f83caeb1fc281326e4f41c058f
Sachin Vishaul <baskar.sa@northeastern.edu>
2025-05-16 19:55:52 -0400

    SDLC: IMPLEMENT — Add return-book UI logic

--- frontend/script.js
+++ frontend/script.js
@@ -316,3 +316,4 @@ document.getElementById('sign-out-btn').addEventListener('click', () => {
 // Add fetch-overdue UI logic
 // Add add-book UI logic
 // Add borrow-book UI logic
+// Add return-book UI logic
